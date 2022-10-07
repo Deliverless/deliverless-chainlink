1251d40a3e89a60a0733f25fd6da7ae5fcf978c0
Chris Couto <couchris@sheridancollege.ca>
2022-10-07 00:00:18 -0400

    isntall strip, add stripe function

---
+++ adapters/bigchaindb-utils/scripts/stripe.js
@@ -0,0 +1,15 @@
+const stripe = require("stripe")('sk_test_51LiTBOHlhPKJMrfBnCyVBdvKHPFoO6dQIQ88KiNZpNauIYMr9a7x10rLUgOrzJ3w3SqInYruHKQoB0WbRqU9smf0000wjTos2s');
+
+export const stripeMethod = async (total) => {
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: "cad",
+    automatic_payment_methods: {
+      enabled: true,
+    },
+  });
+
+return paymentIntent.client_secret
+
+}
